4296a722f6a30d83dedbd847fce96675313f665f
Juhy3 <81273624+juhye-s@users.noreply.github.com>
2021-12-02 19:03:59 +0900

    Update Homework9.java

--- Homework9.java
+++ Homework9.java
@@ -27,19 +27,18 @@ class Homework9{
 
 
 
-        while(true) { //c는1 ,java는 true java는 1을 true로 인식하지 않는다.
+        while(true) { 
             System.out.println("id와 password를 입력해주세요.");
-            System.out.print("id: "); //여기까지 하면 프롬프트창에 입력이 안됨
+            System.out.print("id: "); 
             Scanner sc2 = new Scanner(System.in);
-            String id = sc2.next(); //이게 되야 입력이 됨
+            String id = sc2.next();
 
             if (wc.containsKey(id)) {
                 System.out.print("password: ");
-                //scanner 또 쓸필요 없다
+               
                 String password = sc2.next();
 
-                if (wc.get(id).equals(password)) {//key 에 해당하는 value를 가져올때는 get을 씀 /변수명이니까password괄화안해됨 /key:Id
-                    //equals가 함수라서 ()안에 password넣어야함
+                if (wc.get(id).equals(password)) {
                     System.out.println("id와 비밀번호가 일치합니다.");
                     break;
                 } else {
